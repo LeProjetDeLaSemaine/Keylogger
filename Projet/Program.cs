dfabc6a201d21bd1f90f1883bfad8ec536861848
LeProjetDeLaSemaine <leprojetdelasemaine@gmail.com>
2015-03-08 19:31:10 +0100

    Update Program.cs

--- Projet/Program.cs
+++ Projet/Program.cs
@@ -1,7 +1,7 @@
-﻿//Copyright © 2015 Ernest Foussard
+//Copyright © 2015 Ernest Foussard
 //
 //This file is part of Keylogger.
-<<<<<<< HEAD
+
 // Keylogger is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, version 3 of the License.
@@ -11,22 +11,7 @@
 // GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License
 // along with Keylogger. If not, see <http://www.gnu.org/licenses/>.
-=======
 
-//    Keylogger is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, version 3 of the License.
-
-//    Keylogger is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-
-//    You should have received a copy of the GNU General Public License
-//    along with Keylogger.  If not, see <http://www.gnu.org/licenses/>.
-
-
->>>>>>> origin/master
 
 using System;
 using System.Runtime.InteropServices;
